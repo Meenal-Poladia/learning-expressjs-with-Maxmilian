6ee37977c5d27932cffe6d0dc1a0bb48a07d524e
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-03-12 13:32:05 -0400

    Created routes for adding new functionalities

---
+++ controllers/shop.js
@@ -0,0 +1,38 @@
+const Product = require("../models/product");
+
+exports.getProducts = (req, res, next) => {
+    Product.fetchAll((products) => {
+        res.render('shop/productList', {
+            prods: products,
+            pageTitle: 'Shop',
+            path: '/',
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true
+        });
+    });
+}
+
+exports.getIndex = (req, res, next) => {
+    Product.fetchAll((products) => {
+        res.render('shop/index', {
+            prods: products,
+            pageTitle: 'All products',
+            path: '/products',
+        });
+    });
+}
+
+exports.getCart = (req, res, next) => {
+    res.render("shop/cart", {
+        path: "/cart",
+        pageTitle: "Your Cart"
+    })
+}
+
+exports.getCheckout = (req, res,next) => {
+    res.render("shop/checkout", {
+        path: "/checkout",
+        pageTitle: "Checkout"
+    })
+}
