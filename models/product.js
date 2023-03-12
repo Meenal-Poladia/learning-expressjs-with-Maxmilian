d374fe9579cd50e1d53bf3bea4eed56266f6cd2a
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-03-12 12:12:31 -0400

    Added a fs methods like writeFile and readFile to store and retrieve data

--- models/product.js
+++ models/product.js
@@ -1,4 +1,18 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json'
+);
+
+const getProductsFromFile = (callback) => {
+    fs.readFile(p, (error, fileContent) => {
+        if (error) callback([])
+        else callback(JSON.parse(fileContent));
+    })
+}
 
 module.exports = class Product {
     constructor(title) {
@@ -6,10 +20,15 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this);
+        getProductsFromFile(products => {
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), err => {
+                console.log(`In writeFile`,err);
+            });
+        })
     }
 
-    static fetchAll() {
-        return products;
+    static fetchAll(callback) {
+        getProductsFromFile(callback);
     }
 }
