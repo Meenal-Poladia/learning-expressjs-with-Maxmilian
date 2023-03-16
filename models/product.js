3790e716e8b9ab229ef331806ab4ce227c25cd0f
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-03-16 18:04:33 -0400

    Added data to function findById

--- models/product.js
+++ models/product.js
@@ -21,11 +21,11 @@ module.exports = class Product {
 
     }
 
-    static fetchAll(callback) {
+    static fetchAll() {
         return db.execute("SELECT * FROM products");
     }
 
-    static findById(id, callback){
-
+    static findById(id){
+        return db.execute(`SELECT * FROM products WHERE products.id = ?`, [id]);
     }
 }
