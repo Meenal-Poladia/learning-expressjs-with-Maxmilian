383f088b67f442f037df639bd40365f446d0e20d
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-03-16 15:09:14 -0400

    Connected our application to a database

--- app.js
+++ app.js
@@ -10,12 +10,19 @@ app.set('views', 'views');
 const adminRoutes = require("./routes/admin");
 const shopRoute = require("./routes/shop");
 const errorController = require("./controllers/error");
+const db = require("./utils/database");
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoute);
+
+db.execute("SELECT * FROM products")
+    .then((result) => {
+        console.log(result[0], result[1]);})
+    .catch((error) => {
+        console.log(error);})
 
 app.use(errorController.get404);
 
