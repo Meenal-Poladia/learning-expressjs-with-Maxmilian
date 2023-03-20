37e13fd2db35ae8c4b321cd54f3b4795a2a6a39a
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-03-20 10:45:57 -0400

    Added functionality to create new product in the db

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,11 +13,15 @@ exports.postAddProduct = (req, res, next) => {
     const description = req.body.description;
     const imageUrl = req.body.imageUrl;
     const price = Number(req.body.price);
-    const product = new Product(null, title, imageUrl, description, price);
-    product.save()
-        .then(() => {
-            res.redirect('/');
-        })
+    Product.create({
+        title: title,
+        imageUrl: imageUrl,
+        price: price,
+        description: description,
+    })
+        .then((result => {
+            console.log(`Created the product`);
+        }))
         .catch((error) => {
             console.log(error);
         })
