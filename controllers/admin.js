01b1c9b054afb4db4302398cde7fdd896010d4be
Meenal-Poladia <meenal.poladia94@gmail.com>
2023-03-20 11:39:12 -0400

    Fetching the products on the admin page

--- controllers/admin.js
+++ controllers/admin.js
@@ -54,13 +54,14 @@ exports.postEditProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-    Product.fetchAll((products) => {
-        res.render('admin/products', {
-            prods: products,
-            pageTitle: 'Admin Products',
-            path: '/admin/products',
-        });
-    });
+    Product.findAll()
+        .then(products => {
+            res.render('admin/products', {
+                prods: products,
+                pageTitle: 'Admin Products',
+                path: '/admin/products',
+            });
+        })
 }
 
 exports.deleteProduct = (req, res, next) => {
